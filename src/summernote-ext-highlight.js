5d36fabc1c0619734087f2276c3147dcafdf129c
Ekrem KAYA <webmaster@e-piksel.com>
2015-12-07 20:07:02 +0200

    Fixed add code (e.g. php code) Fixed Language Load Added Turkish Language Added New lang option

--- src/summernote-ext-highlight.js
+++ src/summernote-ext-highlight.js
@@ -31,7 +31,7 @@
      */
     var createCodeNode = function (code, select) {
         var $code = $('<code>');
-        $code.html(code);
+        $code.html(code.replace(/</g,"&lt;").replace(/>/g,"&gt;"));
         $code.addClass('language-' + select);
 
         var $pre = $('<pre>');
@@ -102,10 +102,9 @@
          * @property {Function} buttons.highlight   function to make button
          */
         buttons: { // buttons
-            highlight: function (lang) {
-                return tmpl.iconButton('fa fa-file-code-o', {
+            highlight: function (lang, options) {
+                return tmpl.iconButton(options.iconPrefix + 'file-code-o', {
                     event: 'highlight',
-                    title: 'highlight',
                     title: lang.highlight.highlight,
                     hide: true
                 });
@@ -119,30 +118,49 @@
             highlight: function (lang) {
                 var body = '<div class="form-group">' +
                     '<select class="form-control note-highlight-select">' +
-                    '<option value="bsh">bsh</option>' +
-                    '<option value="c">c</option>' +
-                    '<option value="cc">cc</option>' +
-                    '<option value="cpp">cpp</option>' +
-                    '<option value="cs">cs</option>' +
-                    '<option value="csh">csh</option>' +
-                    '<option value="cyc">cyc</option>' +
-                    '<option value="cv">cv</option>' +
+                    '<option value="apoll">apoll</option>' +
+                    '<option value="basic">basic</option>' +
+                    '<option value="clj">clj</option>' +
+                    '<option value="coffee">coffee</option>' +
+                    '<option value="css">css</option>' +
+                    '<option value="dart">dart</option>' +
+                    '<option value="erlan">erlan</option>' +
+                    '<option value="go">go</option>' +
+                    '<option value="hs">hs</option>' +
                     '<option value="htm">htm</option>' +
                     '<option value="html">html</option>' +
                     '<option value="java">java</option>' +
                     '<option value="js">js</option>' +
-                    '<option value="m">m</option>' +
-                    '<option value="mxml">mxml</option>' +
+                    '<option value="lasso">lasso</option>' +
+                    '<option value="lisp">lisp</option>' +
+                    '<option value="llvm">llvm</option>' +
+                    '<option value="logta">logta</option>' +
+                    '<option value="lua">lua</option>' +
+                    '<option value="matla">matla</option>' +
+                    '<option value="ml">ml</option>' +
+                    '<option value="mumps">mumps</option>' +
+                    '<option value="n">n</option>' +
+                    '<option value="pasca">pasca</option>' +
                     '<option value="perl">perl</option>' +
-                    '<option value="pl">pl</option>' +
-                    '<option value="pm">pm</option>' +
-                    '<option value="py">py</option>' +
                     '<option value="php">php</option>' +
+                    '<option value="proto">proto</option>' +
+                    '<option value="py">py</option>'
+                    '<option value="r">r</option>' +
                     '<option value="rb">rb</option>' +
-                    '<option value="sh">sh</option>' +
+                    '<option value="rd">rd</option>' +
+                    '<option value="rust">rust</option>' +
+                    '<option value="scala">scala</option>' +
+                    '<option value="sql">sql</option>' +
+                    '<option value="swift">swift</option>' +
+                    '<option value="tcl">tcl</option>' +
+                    '<option value="tex">tex</option>' +
+                    '<option value="vb">vb</option>' +
+                    '<option value="vhdl">vhdl</option>' +
+                    '<option value="wiki">wiki</option>' +
                     '<option value="xhtml">xhtml</option>' +
                     '<option value="xml">xml</option>' +
-                    '<option value="xsl">xsl</option>' +
+                    '<option value="xq">xq</option>' +
+                    '<option value="yaml">yaml</option>' +
                     '</select>' +
                     '</div>' +
                     '<div class="form-group">' +
@@ -222,7 +240,13 @@
             'sr-RS-Latin': {},
             'sv-SE': {},
             'th-TH': {},
-            'tr-TR': {},
+            'tr-TR': {
+                highlight: {
+                    highlight: 'Kod Ekle',
+                    insert: 'Kodu Ekle',
+                    codeLabel: 'Kod parçasını girin'
+                }
+            },
             'uk-UA': {},
             'vi-VN': {},
             'zh-CN': {
